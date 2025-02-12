d91673ceef76aa4459a406aba2aa1c4ffa5d7d4e
daongochuy2516 <97348263+daongochuy2516@users.noreply.github.com>
2025-02-12 11:46:23 +0700

    Update Launcher.cmd

--- Launcher.cmd
+++ Launcher.cmd
@@ -1,10 +1,28 @@
 @echo off
-net session >nul 2>&1
-if %errorlevel% neq 0 (
-    echo This script requires administrator privileges. Requesting elevated rights...
-    powershell -Command "Start-Process '%~0' -Verb runAs"
+:: BatchGotAdmin
+::-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"="
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
     exit /B
-)
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+::--------------------------------------
 cd /d "%~dp0"
 title NetSupportSchool Admin Launcher
 set "resourcesDir=%~dp0Resources"
